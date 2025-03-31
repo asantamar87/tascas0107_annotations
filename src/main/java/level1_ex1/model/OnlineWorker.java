d1fb647ef71d105bcbf7d6a04e894696b15565d4
aldosantamaria <asantama@jaumebalmes.net>
2025-03-31 14:17:28 +0200

    level 1 reviewed by Toni

--- src/main/java/level1_ex1/model/OnlineWorker.java
+++ src/main/java/level1_ex1/model/OnlineWorker.java
@@ -2,7 +2,7 @@ package level1_ex1.models;
 
 public class OnlineWorker extends Worker {
 
-    public static final double INTERNET_FLAT_RATE = 50.0;
+    private static final double INTERNET_FLAT_RATE = 50.0;
 
     public OnlineWorker(String firstName, String lastName, double hourlyRate) {
         super(firstName, lastName, hourlyRate);
